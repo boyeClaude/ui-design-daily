068fea43fd37576bca15e68010567dd7d71b335d
boyeClaude <boye.claude@gmail.com>
2024-05-20 00:48:04 +0000

    Profile page header UI
    - feat:add custom font and color via tailwind.config file
    - feat:import asset into public folder
    -fix:correct img path in html file

--- profile-page-header/tailwind.config.js
+++ profile-page-header/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ppBlue: "#13143C",
+        ppLightGray: "#DFDFDF",
+        ppLight: "#FFFFFF",
+        ppOrange: "#F17A37",
+        ppLight_2: "#FFFAF2",
+      },
+
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
